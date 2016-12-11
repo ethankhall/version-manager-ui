dcf415e282cadd85e26b48df659c44fe4ea45f87
Ethan Hall <ethan@ehdev.io>
2016-12-11 13:10:44 -0800

    Fixing build again

--- bin/fix-ci-vm.sh
+++ bin/fix-ci-vm.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-sudo apt-get remove python-virtualenv python-openssl python3-openssl
-sudo apt-get update
-sudo apt-get install python-openssl python3-openssl
-sudo rm -rf /opt/google-cloud-sdk/
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-export CLOUDSDK_PYTHON_SITEPACKAGES=1
-curl https://sdk.cloud.google.com | bash
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
 source ~/.bashrc
-export CLOUDSDK_CORE_DISABLE_PROMPTS=0
-
-gcloud --quiet components update
